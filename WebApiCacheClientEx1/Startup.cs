44a66005d428df26128a4efc4508d064fb7c0ffb
Daniel Kemper <kemper001@hotmail.com>
2014-03-15 23:45:51 -0500

    Added OWIN CORS via Startup.cs.

---
+++ WebApiCacheClientEx1/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiCacheClientEx1.Startup))]
+namespace WebApiCacheClientEx1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+        }
+    }
+}

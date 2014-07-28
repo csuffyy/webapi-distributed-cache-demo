1a30dc0ca0de5c18b4549f9f0a1ad11b10721f4b
dkemper01 <kemper001@hotmail.com>
2014-07-27 23:18:09 -0500

    Ditched check for array in worker message processor.
    Ditched check for array in worker message processor.

--- WebApiCacheClientEx1/Scripts/warmup-worker.js
+++ WebApiCacheClientEx1/Scripts/warmup-worker.js
@@ -3,23 +3,21 @@ self.addEventListener('message', function(e) {
 
     var msgData = e.data;
 
-    if ($.isArray(msgData)) {
+    for (var i = 0; i < msgData.length; i++) {
 
-        for (var i = 0; i < msgData.length; i++) {
+        var getUrl = msgData[i];
+        var oReq = new XMLHttpRequest();
 
-            var getUrl = msgData[i];
-            var oReq = new XMLHttpRequest();
+        oReq.open("get", getUrl, false);
+        oReq.send(null);
 
-            oReq.open("get", getUrl, false);
-            oReq.send(null);
-
-            if (oReq.responseText == 'true') {
-                self.postMessage('A web worker just warmed up ' + getUrl + '.');
-            } else {
-                self.postMessage('A problem was encountered trying to warm up ' + getUrl + '.');
-            }
-
+        if (oReq.responseText == 'true') {
+            self.postMessage('A web worker just warmed up ' + getUrl + '.');
+        } else {
+            self.postMessage('A problem was encountered trying to warm up ' + getUrl + '.');
         }
+
     }
+
 
 }, false);

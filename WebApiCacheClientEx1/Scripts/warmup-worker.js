ad295cbf992b0276cad21f66b7b7ed8ca67a7a76
dkemper01 <kemper001@hotmail.com>
2014-07-27 23:31:04 -0500

    Now using friendly domain name to report warmup status.
    Now using friendly domain name to report warmup status.

--- WebApiCacheClientEx1/Scripts/warmup-worker.js
+++ WebApiCacheClientEx1/Scripts/warmup-worker.js
@@ -6,15 +6,17 @@ self.addEventListener('message', function(e) {
     for (var i = 0; i < msgData.length; i++) {
 
         var getUrl = msgData[i];
+        var domainName = getUrl.replace('http://', '').replace('https://', '').split(/[/?#]/)[0];
+
         var oReq = new XMLHttpRequest();
 
         oReq.open("get", getUrl, false);
         oReq.send(null);
-
+        
         if (oReq.responseText == 'true') {
-            self.postMessage('A web worker just warmed up ' + getUrl + '.');
+            self.postMessage('A web worker just warmed up ' + domainName + '.');
         } else {
-            self.postMessage('A problem was encountered trying to warm up ' + getUrl + '.');
+            self.postMessage('A problem was encountered trying to warm up ' + domainName + '.');
         }
 
     }
